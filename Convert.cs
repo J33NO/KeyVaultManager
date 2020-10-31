e25a2296235cbd7e50b7a2ff647a48e7cc79b60c
Daniel Carlson <performancerepair@gmail.com>
2020-10-30 22:34:23 -0700

    Adding ability to load either json or config values into datagrid for manipulation.

--- Convert.cs
+++ Convert.cs
@@ -1,8 +1,10 @@
 ﻿using KeyVaultManager.Models;
+using Microsoft.Win32;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Text;
 
 namespace KeyVaultManager
@@ -10,34 +12,60 @@ namespace KeyVaultManager
     public class Convert
     {
 
-        public static List<KeyVaultModel> ConvertXmlToDictionary(ExeConfigurationFileMap xml)
+        public static List<KeyValueModel> ConvertConfig(ExeConfigurationFileMap xml)
         {
-            List<KeyVaultModel> kvp = new List<KeyVaultModel>();
+            List<KeyValueModel> kvp = new List<KeyValueModel>();
             Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(xml, ConfigurationUserLevel.None);
             KeyValueConfigurationCollection settings = configuration.AppSettings.Settings;
             foreach (KeyValueConfigurationElement item in settings)
             {
-                KeyVaultModel xmlValues = new KeyVaultModel();
-                xmlValues.secretName = item.Key.ToString();
-                xmlValues.secretValue = item.Value.ToString();
+                KeyValueModel xmlValues = new KeyValueModel();
+                xmlValues.key = item.Key.ToString();
+                xmlValues.value = item.Value.ToString();
                 xmlValues.isSelected = false;
                 kvp.Add(xmlValues);
             }
             return kvp;
         }
 
-        public static string ConvertDictionaryToJson(List<KeyVaultModel> kvps)
+        public static string ConvertDictionaryToJson(List<KeyVaultModel> kvms)
         {
-            //List<KeyVaultModel> keyVaultValues = new List<KeyVaultModel>();
-            //foreach (KeyVaultModel kvm in kvps)
-            //{
-            //    KeyVaultModel kvp = new KeyVaultModel();
-            //    kvp.secretName = kvm.secretName;
-            //    kvp.secretValue = kvm.secretValue;
-            //    keyVaultValues.Add(kvp);
-            //}
-            string json = JsonConvert.SerializeObject(kvps, Newtonsoft.Json.Formatting.Indented);
-            return json;
+            try
+            {
+                string json = JsonConvert.SerializeObject(kvms, Newtonsoft.Json.Formatting.Indented);
+                return json;
+
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public static bool SaveJson(string json)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Title = "Json Export";
+                saveFileDialog.RestoreDirectory = true;
+                saveFileDialog.CheckFileExists = false;
+                saveFileDialog.CheckPathExists = false;
+                saveFileDialog.Filter = "All files (*.*)|*.*";
+                saveFileDialog.DefaultExt = ".json";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    File.WriteAllText(saveFileDialog.FileName, json);
+                    return true;
+                }
+                else return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
         }
     }
 }
---
+++ Models/KeyValueModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyVaultManager.Models
+{
+    public class KeyValueModel
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+        public  bool isSelected { get; set; }
+    }
+
+}
